9f63745de4c2171cb007f99da5d059e8385013f0
Betül Ağca <108745065+betulagca@users.noreply.github.com>
2022-09-16 17:21:07 +0300

    Update Account.java

--- ch11hw4/Account.java
+++ ch11hw4/Account.java
@@ -1,1 +1,21 @@
 package ch11hw4;
+public class Account {
+
+	private double balance;
+	
+	public Account() {
+		this.balance= 0;
+		
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(double balance) {
+		this.balance= balance;
+	}
+
+	
+
+}
