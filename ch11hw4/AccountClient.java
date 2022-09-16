257984402ee18044060da4f43f5b4daf37e79e2e
Betül Ağca <108745065+betulagca@users.noreply.github.com>
2022-09-16 17:22:27 +0300

    Update AccountClient.java

--- ch11hw4/AccountClient.java
+++ ch11hw4/AccountClient.java
@@ -1,13 +1,26 @@
 package ch11hw4;
-public class AccountTest {
-    public static void main(String[] args) {
-        AccountClient accountClient = new AccountClient();
-        Account from = new Account();
-        Account to = new Account();
-
-        accountClient.withDraw(from, 100.0);
-        accountClient.deposit(from, 500.0);
-        accountClient.deposit(to, 100.0);
-        accountClient.transfer(to, from, 500.0);
-    }
+public class AccountClient {
+	double amount;
+	double transferCost;
+	public void withDraw(Account account, double amount) {
+		   if (amount > account.getBalance() ) {
+			   System.out.println("Yetersiz Bakiye");
+				return;
+		   }
+		   System.out.println(amount + "Para yatirma isleminiz basariyla gerceklesti");
+	}
+	 public void deposit(Account account, double amount) {
+		 System.out.println(amount + "Para cekme isleminiz basariyla gerceklesti");
+		 return;
+	 }
+	 public void transfer(Account from, Account to, double account) {
+		 if(from.getBalance()< (amount + transferCost)){
+		 System.out.println("Yetersiz Bakiye");
+		 return;
+	 }
+	 
+	 System.out.println(amount + "Para transfer isleminiz basariyla gerceklesti");
+	 
+	 }
+	 
 }
