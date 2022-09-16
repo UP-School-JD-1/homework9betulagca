278c98147c5c7ac87bd185cd5490b563f5dd1838
Betül Ağca <108745065+betulagca@users.noreply.github.com>
2022-09-16 17:22:03 +0300

    Update AccountTest.java

--- ch11hw4/AccountTest.java
+++ ch11hw4/AccountTest.java
@@ -1,1 +1,13 @@
 package ch11hw4;
+public class AccountTest {
+    public static void main(String[] args) {
+        AccountClient accountClient = new AccountClient();
+        Account from = new Account();
+        Account to = new Account();
+
+        accountClient.withDraw(from, 100.0);
+        accountClient.deposit(from, 500.0);
+        accountClient.deposit(to, 100.0);
+        accountClient.transfer(to, from, 500.0);
+    }
+}
